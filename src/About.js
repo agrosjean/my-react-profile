55741f3e7675595bcba1129d35969d477c5352f6
agrosjean <agrosjean007@gmail.com>
2021-07-12 17:08:32 -0400

    Add Home, About and Portfolio starter components

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    // TODO - Add html with photo, short bio
+    render() {
+        return (
+            <div>
+                <h3>About</h3>
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ src/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Portfolio extends Component {
+    // TODO - Add html with 6 projects, images, github link
+    render() {
+        return (
+            <div>
+                <h3>Portfolio</h3>
+            </div>
+        )
+    }
+}
+
+export default Portfolio;

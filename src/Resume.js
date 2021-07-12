3c5d69833ef32adeef571b3ddb857f57861941c8
agrosjean <agrosjean007@gmail.com>
2021-07-12 17:08:46 -0400

    Add Resume component

---
+++ src/Resume.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Resume extends Component {
+    // TODO - Add html for downloadble resume and list of proeficencies
+    render() {
+        return (
+            <div>
+                <h3>Resume</h3>
+            </div>
+        )
+    }
+}
+
+export default Resume;
